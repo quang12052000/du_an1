19b59c927638d5edcd3e227a832814f3acbed7ec
ptynayvan1 <74031512+ptynayvan1@users.noreply.github.com>
2020-11-18 08:02:04 +0700

    ok

--- site/models/index.php
+++ site/models/index.php
@@ -5,7 +5,11 @@ $sql="SELECT * FROM `xe`";
 return query($sql);
 }
 function ctxe($id){
-    $sql="SELECT * FROM `xe` WHERE id_xe=$id";
+    $sql="SELECT * FROM `xe` INNER JOIN loai_xe ON loai_xe.Id_loaixe=xe.Id_loaixe WHERE id_xe=$id";
+    return queryOne($sql);
+}
+function checkdn($user,$pass){
+    $sql="SELECT * FROM nguoi_dung WHERE ten_dangnhap='$user' AND mat_khau='$pass'";
     return queryOne($sql);
 }
 ?>
